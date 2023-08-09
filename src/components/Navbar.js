445f49b56e083c11269fb6ff824ad71d79a9c42d
Sachin Vishwakarma <sachin.vishwakarma@telusinternational.com>
2023-08-09 15:48:16 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <nav className='navbar'>
+      <div className='navbar-brand'>
+        <a id='logo' href='/'>Telus E-Cart</a>
+      </div>
+      <div className={`navbar-links ${isMobileMenuOpen ? 'mobile-open' : ''}`}>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/service'>Service</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/register'>
+          <button>Sign Up</button>
+        </Link>
+      </div>
+      <div className='mobile-toggle' onClick={toggleMobileMenu}>
+        <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`}></div>
+        <div className={`bar ${isMobileMenuOpen ? 'open' : ''}`}></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      <h3>
+        This is the Service page
+      </h3>
+      
+    </div>
+  )
+}
+
+export default Services
